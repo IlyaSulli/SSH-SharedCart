5712abcda98a6909993d2e99662c3806bee10e43
Rokkema <rokkemagaming@gmail.com>
2024-12-05 23:46:47 +0000

    Populate page with searched items

--- client/js/shop.js
+++ client/js/shop.js
@@ -1,4 +1,8 @@
 let currentShop = "tesco";
+let itemsArray = [];
+let searchTerms = "";
+let currentPage,
+	maxPage = 1;
 
 // Open and close dropdown on clicking the logo
 document.getElementById("shopLogoBackground").addEventListener("click", () => {
@@ -26,22 +30,100 @@ document.querySelectorAll(".shopOption").forEach((el) => {
 	});
 });
 
-const search = async () => {
-	const searchTerms = document
-		.querySelector(".searchBar")
-		.value.replace(/\s+/g, "-"); // Replace whitespace characters
-	const shopId = localStorage.getItem("shopId");
-
-	axios
-		.get(`/search/${shopId}/?searchTerm=${searchTerms}`)
+const renderShopPage = async () => {
+	await axios
+		.get(
+			`/search/${localStorage.getItem(
+				"shopId"
+			)}/?searchTerm=${searchTerms}&page=${currentPage}&limit=5`
+		)
 		.then((res) => {
-			console.log(res);
+			itemsArray = res.data.data.items;
+			document.querySelector(".itemList").innerHTML = "";
+			itemsArray.forEach((item) => {
+				const itemId = item._id;
+				const name = item.ItemName;
+				const price = item.ItemCost;
+				const imageSrc = item.ImageLink;
+				const description = item.ItemDescription;
+
+				const itemDiv = `
+		<div class="itemContainer layer2container" id="${itemId}">
+				<div class="itemAdvertisement">
+					<div class="itemImageContainer layer3container">
+						<div class="itemImage"><img src="${imageSrc}" alt="Picture of ${name}"></div>
+					</div>
+					<div class="itemMeta">
+						<h4 class="itemName">
+							${name}
+						</h4>
+						<p class="itemDescription">
+							${description}
+						</p>
+					</div>
+				</div>
+				<div class="itemPurchasing">
+					<span class="itemPrice itemPriceText">£${price}</span>
+					<label
+						>Quantity:
+						<input
+							type="number"
+							class="itemQuantity layer3container"
+							name="quantity"
+							min="1"
+							max="99"
+					/></label>
+					<input
+						type="submit"
+						class="addButton buttonText layer3container"
+						value="Add"
+					/>
+				</div>
+			</div>`;
+				document.querySelector(".itemList").appendChild(itemDiv);
+			});
 		})
 		.catch(function (error) {
-			// handle error
 			console.error(error);
 		});
 };
+
+const search = async () => {
+	currentPage = 1;
+	searchTerms = document
+		.querySelector(".searchBar")
+		.value.replace(/\s+/g, "-"); // Replacing Whitespace characters
+
+	// Setting maxPage
+	await axios
+		.get(
+			`/search/${localStorage.getItem(
+				"searchTerms"
+			)}/?searchTerm=${localStorage.getItem("shopId")}`
+		)
+		.then((res) => {
+			maxPage = Math.ceil(res.data.results / 5);
+		})
+		.catch(function (error) {
+			console.error(error);
+		});
+
+	renderShopPage();
+};
+
+document.getElementById("previous").addEventListener("click", () => {
+	if (currentPage > 1) {
+		currentPage--;
+		renderShopPage();
+	}
+});
+
+document.getElementById("next").addEventListener("click", () => {
+	if (currentPage < maxPage) {
+		currentPage++;
+		renderShopPage();
+	}
+});
 
 document
 	.getElementById("searchButton")
