a325b0fab66600faaa7c986603633d0762fcc95a
Hector Warner <154226196+Hector-Warner@users.noreply.github.com>
2024-12-07 02:09:39 +0000

    Update cartController.js

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -162,6 +162,7 @@ exports.getShops = async (req, res) => {
 exports.getSelectedShop = async (req, res) => {
    try {
       const items = await PersonItem.find();
+      console.log("hello");
       if (items.length != 0) {
          const itemId = items[0].ItemId;
          const shopId = await Item.findById(itemId);
