fbb9482f8c84f9bf9751dc2e69610dc1235ea1c6
Ilya <ilyasullivan01@gmail.com>
2024-12-07 02:01:15 +0000

    Update to cart.js
    Additional formatting and corrections to the api calls and formatting based on the server's needs

--- client/js/cart.js
+++ client/js/cart.js
@@ -1,11 +1,9 @@
 function renderHero(shopName, shopImage, shopColor) {
-  if (shopName !== "") {
-    const shopBackground = document.getElementById("shopLogoBackground");
-    shopBackground.style.backgroundColor = shopColor;
-    const shopLogo = document.getElementById("shopLogo");
-    shopLogo.src = shopImage;
-    shopLogo.alt = shopName + " Logo";
-  }
+  const shopBackground = document.getElementById("shopLogoBackground");
+  shopBackground.style.backgroundColor = `#${shopColor}`;
+  const shopLogo = document.getElementById("shopLogo");
+  shopLogo.style.backgroundImage = `url(${shopImage})`;
+  shopLogo.alt = `${shopName} Logo`;
 }
 
 function renderCart(
@@ -27,14 +25,19 @@ function renderCart(
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-360 280-560h400L480-360Z"/></svg>
         </span>
         <h3 class="cartPersonName">${firstname + " " + lastname}</h3>
-        <h4 class="cartPersonTotal">£${cartSubtotal}</h4>
+        <h4 class="cartPersonTotal">£${cartSubtotal.toFixed(2)}</h4>
         <button class="cartConfirmButton layer2container buttonText ${
           confirmed ? "confirmed" : ""
         } confirmButton" disabled>
-            ${confirmed ? "Confirmed" : "Awaiting Confirmation"}
+        ${confirmed ? "Confirmed" : "Awaiting Confirmation"}
         </button>
     `;
   container.appendChild(summaryDiv);
+
+    // Accordian dropdown functionality
+    container.addEventListener("click", () => {
+      container.classList.toggle("expanded");
+    });
 
   // Create the dropdown section
   const dropdownDiv = document.createElement("div");
@@ -47,42 +50,38 @@ function renderCart(
   // Loop through cart items
   for (let i = 0; i < cartInfo.length; i++) {
     const item = cartInfo[i];
+    console.log(item);
     const itemDiv = document.createElement("div");
     itemDiv.classList.add("itemContainer", "layer2container");
     itemDiv.innerHTML = `
               <div class="itemImageContainer layer3container">
                   <div class="itemImage" style="background-image: url('${
-                    item.image_link
+                    item.ImageLink
                   }');"></div>
               </div>
               <div class="itemMeta">
-                  <h4 class="itemName">${item.name}</h4>
-                  <p class="itemDescription">${item.description}</p>
+                  <h4 class="itemName">${item.ItemName}</h4>
+                  <p class="itemDescription">${item.ItemDescription}</p>
               </div>
-              <span class="itemPrice itemPriceText">£${item.price.toFixed(
-                2
-              )}</span>
+              <span class="itemPrice itemPriceText">£${item.ItemCost.toFixed(2)}</span>
               <label>Quantity: <input type="number" class="itemQuantity layer3container" name="quantity" min="1" max="99" value="${
-                item.quantity
-              }" disabled data-itemid="${key}"></label>
-              <input type="submit" class="removeButton buttonText layer3container" value="Remove" disabled data-itemid="${key}">
+                item.Quantity.Quantity
+              }" disabled data-itemid="${item._id}"></label>
+              <input type="submit" class="removeButton buttonText layer3container" value="Remove" disabled data-itemid="${item._id}">
             `;
     itemList.appendChild(itemDiv);
 
     // Handle quantity updates
-
     const quantityInput = itemDiv.querySelector(".itemQuantity");
     quantityInput.addEventListener("change", async (event) => {
       let newQuantity = event.target.value;
-
-      // Call the quantity update function
       newQuantity = await sendAPIRequest("updateQuantity", {
         userId: userId,
         itemId: item._id,
         newQuantity: newQuantity,
       });
 
-      if (isUpdated) {
+      if (newQuantity) {
         console.log(`Quantity for item ${item._id} updated to ${newQuantity}`);
         quantityInput.value = newQuantity;
       } else {
@@ -91,16 +90,18 @@ function renderCart(
     });
 
     // Handle item Removal
-
-    const removeButton = itemDiv.getElementsByClassName("removeButton");
+    const removeButton = itemDiv.querySelector(".removeButton");
     removeButton.addEventListener("click", async (event) => {
       const isRemoved = await sendAPIRequest("removeItem", {
         userId: userId,
         itemId: item._id,
       });
+
       if (isRemoved) {
         console.log(`Removed item ${item._id}`);
         itemDiv.remove();
+      } else {
+        console.error(`Failed to remove item ${item._id}`);
       }
     });
   }
@@ -113,17 +114,17 @@ function renderCart(
     <div class="personalCartCheckout">
         <div class="personalCartTotal personalCartTotalItem">
             <span class="personalCartTotalName">Cart Total</span>
-            <span class="personalCartTotalResult">£${cartSubtotal}</span>
+            <span class="personalCartTotalResult">£${parseFloat(cartSubtotal).toFixed(2)}</span>
         </div>
         <div class="personalDeliveryFee personalCartTotalItem">
             <span class="personalDeliveryFeeName">Delivery Fee Split</span>
-            <span class="personalDeliveryFeeResult">£${deliveryFeeSplit}</span>
+            <span class="personalDeliveryFeeResult">£${parseFloat(deliveryFeeSplit).toFixed(2)}</span>
         </div>
         <hr class="personalDivider ">
         <div class="personalTotal personalCartTotalItem">
             <span class="personalTotalName">Your Total</span>
             <span class="personalTotalResult">£${
-              cartSubtotal + deliveryFeeSplit
+              (parseFloat(cartSubtotal) + parseFloat(deliveryFeeSplit)).toFixed(2)
             }</span>
         </div>
     </div>
@@ -206,17 +207,17 @@ function renderCheckout(subtotal, deliveryFee) {
                     <h4 id="cartTotalName">
                         Cart Total:
                     </h4>
-                    <h4 id="cartTotalResult">£${subtotal}</h4>
+                    <h4 id="cartTotalResult">£${subtotal.toFixed(2)}</h4>
                 </div>
                 <div id="deliveryFeeTotal" class="checkoutItem">
                     <h4 id="deliveryFeeTotalName">
                         Delivery Fee:
                     </h4>
-                    <h4 id="deliveryFeeTotalResult">£${deliveryFee}</h4>
+                    <h4 id="deliveryFeeTotalResult">£${deliveryFee.toFixed(2)}</h4>
                 </div>
                 <div id="totalPrice" class="checkoutItem">
                     <h3 id="totalPriceTotal">Total</h3>
-                    <h3 id="totalPriceResult">£${subtotal + deliveryFee}</h3>
+                    <h3 id="totalPriceResult">£${(parseFloat(subtotal) + parseFloat(deliveryFee)).toFixed(2)}</h3>
                 </div>
             </div>
     `;
@@ -224,10 +225,11 @@ function renderCheckout(subtotal, deliveryFee) {
 
 function calculatePersonalCartSubtotal(cart) {
   let total = 0.0;
-  for (let i = 0; i < cart.length; i++) {
-    let item = cart[i];
-    total += item.price;
-  }
+  cart.forEach((item) => {
+    console.log(item);
+    total += item.ItemCost*item.Quantity.Quantity;
+  });
+  console.log(`Subtotal: £${total.toFixed(2)}`);
   return total;
 }
 
@@ -254,48 +256,46 @@ async function sendAPIRequest(endpoint, data) {
 
 async function getAPIRequest(endpoint, data) {
   try {
-    await axios
-      .get("http://localhost:5500/getCart/?")
-      .then((res) => {
-        console.log(res.data.data);
-        return res.data.data;
-      })
-      .catch(function (error) {
-        renderErrorCart(error.errorCode, error.errorMessage);
-      });
+    const response = await axios.get(`http://localhost:5500/${endpoint}/?${data}`);
+    return response.data.data;
   } catch (error) {
-    renderErrorCart(500, "Internal Server Error");
-    console.error("Failed to send data:", error);
+    if (error.response) {
+      renderErrorCart(error.response.status, error.response.statusText);
+    } else {
+      renderErrorCart(500, "Internal Server Error");
+    }
+    console.error(`Failed to send data for API ${endpoint}:`, error);
   }
 }
 
 // On page load, build the cart page
 document.addEventListener("DOMContentLoaded", async function () {
   let carts = await getAPIRequest("getCart", "");
-  if (carts.length === 0) {
+  if (carts.people.length === 0) {
     // If there are no user's in the cart, render the empty cart HTML
     renderEmptyCart();
   } else {
-    let shop = await sendAPIRequest("getSelectedShop", {});
-    renderHero(shop.name, shop.image, shop.color);
-    const deliveryFeeSplit = (shop.deliveryfee / carts.length).toFixed(2);
+    let shop = await getAPIRequest("getCart/getSelectedShop", "");
+    renderHero(shop.shop.shopName, shop.shop.DeliveryLogo, shop.shop.Colour);
+    const deliveryFeeSplit = (shop.shop.DeliveryPrice / carts.people.length).toFixed(2);
     let cartsSubtotal = 0;
     // For each user with items in the cart, render their personal cart
-    carts.forEach((cart) => {
-      const subtotal = calculatePersonalCartSubtotal(cart.cart);
+    carts.people.forEach((cart) => {
+      let subtotal = calculatePersonalCartSubtotal(JSON.parse(cart.Cart));
       cartsSubtotal += subtotal;
+      
       renderCart(
-        cart.userid,
-        cart.firstname,
-        cart.lastname,
-        cart.cart,
+        cart._id,
+        cart.FirstName,
+        cart.Surname,
+        JSON.parse(cart.Cart),
         deliveryFeeSplit,
         subtotal,
-        cart.confirmed
+        cart.Confirmed
       );
     });
     let selectedID = localStorage.getItem("selectedID");
     modifySelectedCart(selectedID); // Update the selected user to be able to confirm and update items for themselves
-    renderCheckout(cartsSubtotal, shop.deliveryfee);
+    renderCheckout(cartsSubtotal, shop.shop.DeliveryPrice);
   }
 });
