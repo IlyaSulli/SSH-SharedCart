e969c41e320fc4a6f54e5601e79b0c256db69333
Coll MacCulloch <coll.macculloch@gmail.com>
2024-12-09 20:21:34 +0000

    server.js error catch on no config file

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const app = require(`${__dirname}/server/app`);
+const app = require(`${__dirname}/server/app.js`);
 
 dotenv.config({ path: `${__dirname}/server/config.env` });
 
-const DB = process.env.MONGODB_URI.replace(
-  "<password>",
-  process.env.DATABASE_PASSWORD
-);
+console.log(process.env.MONGODB_URI);
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(console.log("DB connection complete"));
+if (typeof process.env.MONGODB_URI !== "undefined") {
+  const DB = process.env.MONGODB_URI.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD
+  );
+
+  mongoose
+    .connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("DB connection complete"));
+}
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
